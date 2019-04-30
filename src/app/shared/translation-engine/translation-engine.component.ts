ddddb19d57cbcf87e6504c75e52f6a356cc3da8d
Asdosh Dev <karthikdev27@gmail.com>
2019-04-30 21:42:26 +0530

    parse and display text

--- src/app/shared/translation-engine/translation-engine.component.ts
+++ src/app/shared/translation-engine/translation-engine.component.ts
@@ -1,8 +1,6 @@
 import { TranslationEngineService } from './translation-engine.service';
-import { Component, OnInit, Input , Output,EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { parse } from 'papaparse';
-
-
 
 @Component({
   selector: 'translation-engine',
@@ -11,31 +9,58 @@ import { parse } from 'papaparse';
 })
 export class TranslationEngineComponent implements OnInit {
 
-  constructor(private service:TranslationEngineService) { }
+  constructor(private service: TranslationEngineService) { }
   csvResults;
+  modifiedResults;
+  langArray;
+  finalValue = {};
   @Output() sendLanguages = new EventEmitter();
 
   ngOnInit() {
+    let modifiedResults;
     let csvFilePath = "./../../../assets/lang/aiesec.csv";
     parse(csvFilePath, {
       download: true,
       complete: (result) => {
-        let modifiedResults = this.modifyResults(result.data)
+        console.log(result.data);
+        window['myarray'] = this.csvResults = result.data;
+        this.modifiedResults = this.modifyResults(result.data);
+        this.service.getLang.subscribe(data => {
+          this.translateText(data);
+        });
       }
-    });
-
-    this.service.getLang.subscribe(data =>{
-      debugger;
-      this.translateText(data);
     });
   }
 
   modifyResults(results) {
-    let langArray = results[0];
-    this.sendLanguages.emit(langArray);
+    //emit the languages to dropdown
+    this.langArray = results[0];
+    this.sendLanguages.emit(this.langArray);
+    return results.splice(1);
   }
 
-  translateText(lang){
+
+
+  translateText(lang) {
+    let mainIndex = this.langArray.indexOf(lang);
+    let obj = {};
+    let arrayOfValues = this.modifiedResults.map((item, index) => {
+      // let key = 'randomKey'+index;
+      return (item[mainIndex]);
+    });
+
+    console.log(arrayOfValues);
+
+
+    
+    console.log("CHANGED RESULT");
+    
+    let arr = [];
+    let arrayToObject = (arr, keyField) => {
+      window['Object'].assign({}, ...arr.map((item,index) =>{ console.log(({[keyField+'_' +index]: item})); ({[keyField+'_' +index]: item}) } ));
+    }
+    
+    console.log(arrayToObject(arrayOfValues, "langText"));
     console.log("TRANSLATING TEXT to " + lang);
   }
 }
