e403afb7744c061624ff508c94308c46640b34b9
Asdosh Dev <karthikdev27@gmail.com>
2019-05-02 12:46:41 +0530

    heroku setup

---
+++ server.js
@@ -0,0 +1,11 @@
+// server.js
+const express = require('express');
+const app = express();
+var path = require('path');
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(path.join(__dirname, 'dist')));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
+
