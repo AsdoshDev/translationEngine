2d345fa0bb38d059c8f27f6a5975778b14acd99d
Asdosh Dev <karthikdev27@gmail.com>
2019-05-02 13:44:17 +0530

    test_comp

--- server.js
+++ server.js
@@ -2,10 +2,13 @@
 const express = require('express');
 const app = express();
 var path = require('path');
+const compression = require('compression');
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(path.join(__dirname, 'dist')));
+app.use(compression());
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
+
 
